3ec4cd15317b8611c4c21c07ec0136a5591f4103
Tiago Neves <rykarumstuler@gmail.com>
2021-03-17 02:28:55 -0300

    Utilizando o conceito de props

--- iniciando_com_reactjs/app-react/src/Props.js
+++ iniciando_com_reactjs/app-react/src/Props.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Props extends Component {
-    constructor() {
-        this.props = {
-            
-        };
+    constructor(props) {
+        super(props);
+        const { attr, attr2, attr3 } = this.props;
+        console.log(attr, attr2, attr3);
     }
 
-
     render() {
-        return (
+        const { attr, attr2, attr3 } = this.props;
 
+
+        return (
+            <div>
+                { attr === 'p' ? <p>PROPS</p>: <h1>PROPS</h1> }
+            </div>
         );
     }
 }
